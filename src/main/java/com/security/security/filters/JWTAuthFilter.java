3442e9678ef10750adbe8a2375fa5b5353fe9880
sachinthya <ssrathnavibushana@gmail.com>
2025-04-26 17:32:19 +0530

    remove TODO tag

--- src/main/java/com/security/security/filters/JWTAuthFilter.java
+++ src/main/java/com/security/security/filters/JWTAuthFilter.java
@@ -26,7 +26,7 @@ public class JWTAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //TODO check token
+        //- check token
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
@@ -35,21 +35,21 @@ public class JWTAuthFilter extends OncePerRequestFilter {
             token = authHeader.substring(7);
             username = jwtUtil.extractUsername(token);
         }
-        //TODO validate token
+        //- validate token
         if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
-            //TODO - for further user verification need to fetch the userdetails
+            //- for further user verification need to fetch the userdetails
             UserDetails userDetails = customUserDetailService.loadUserByUsername(username);
-            //TODO set userdetails into context
+            //- set userdetails into context
             if (jwtUtil.validateToken(username, userDetails, token)){
-                //TODO create authToken with the userdetails (eg:- userDetails, roles,...)
+                //- create authToken with the userdetails (eg:- userDetails, roles,...)
                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-               //TODO give the request details for the authToken
+               //- give the request details for the authToken
                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-               //TODO set the authToken to the context
+               //- set the authToken to the context
                SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-        //TODO after that call to the next filter
+        //- after that call to the next filter
         filterChain.doFilter(request,response);
     }
 }
